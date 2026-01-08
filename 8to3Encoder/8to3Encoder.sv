5fc89397964fc0b74ad0fc1b7949856cca6716b9
Wamiq-Irfan <irfanwamiq3@gmail.com>
2026-01-08 15:28:33 +0500

    Update 8to3Encoder.sv

--- 8to3Encoder/8to3Encoder.sv
+++ 8to3Encoder/8to3Encoder.sv
@@ -1,1 +1,23 @@
+module encoder8to3 (
+    input  logic [7:0] in,
+    output logic [2:0] out,
+    output logic valid
+);
+    always_comb begin
+        valid = 1'b0;
+        out   = 3'b000;
+        if (in[7]) begin out = 3'd7; valid = 1'b1; end
+        else if (in[6]) begin out = 3'd6; valid = 1'b1; end
+        else if (in[5]) begin out = 3'd5; valid = 1'b1; end
+        else if (in[4]) begin out = 3'd4; valid = 1'b1; end
+        else if (in[3]) begin out = 3'd3; valid = 1'b1; end
+        else if (in[2]) begin out = 3'd2; valid = 1'b1; end
+        else if (in[1]) begin out = 3'd1; valid = 1'b1; end
+        else if (in[0]) begin out = 3'd0; valid = 1'b1; end
+        else begin
+            out = 3'b000;
+            valid = 1'b0;
+        end
+    end
+endmodule
 
