36160dfa4b7835abc8d36531c17a48b1fabb1aac
Wamiq-Irfan <irfanwamiq3@gmail.com>
2026-01-08 15:25:05 +0500

    Update Adder32.sv

--- Adder32/Adder32.sv
+++ Adder32/Adder32.sv
@@ -1,1 +1,15 @@
 
+module adder32 (
+    input  logic [31:0] a,
+    input  logic [31:0] b,
+    input  logic cin,
+    output logic [31:0] sum,
+    output logic cout
+);
+    logic [32:0] t_sum;
+    always_comb begin
+        t_sum = a + b + cin;
+        sum  = t_sum[31:0];
+        cout = t_sum[32];
+    end
+endmodule
